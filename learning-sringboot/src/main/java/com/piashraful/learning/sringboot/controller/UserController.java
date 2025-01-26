e981b104c3b795aef12ff9fcffb37a09232935e4
Ashraful Haque Piash <piashrafulhaque@gmail.com>
2025-01-26 21:33:14 +0600

    Initial commit

--- learning-sringboot/src/main/java/com/piashraful/learning/sringboot/controller/UserController.java
+++ learning-sringboot/src/main/java/com/piashraful/learning/sringboot/controller/UserController.java
@@ -5,6 +5,7 @@ import com.piashraful.learning.sringboot.entity.User;
 import com.piashraful.learning.sringboot.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,13 +18,23 @@ public class UserController {
 
 
     @PostMapping("/users")
-    public String addUser(@RequestBody User user){
-        try{
+    public String addUser(@RequestBody User user) {
+        try {
             userService.saveUser(user);
             return ResponseEntity.ok("User added successfully").toString();
-        }catch(Exception e){
+        } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage()).toString();
         }
 
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "This is a test for security";
+    }
+
+    @GetMapping("/test")
+    public String testOne() {
+        return "This is a test for security";
     }
 }
